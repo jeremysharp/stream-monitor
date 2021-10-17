60d728429abbd8c1f0d3d5d6ae0182d86876f2df
Jeremy Sharp <jeremysharp@me.com>
2021-10-17 14:50:04 +0100

    opened to all ips

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ fastify.get('/api/json', async function (request, reply) {
 })
 
 // Run the server!
-fastify.listen(3000, function (err, address) {
+fastify.listen(3000, '0.0.0.0', function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
